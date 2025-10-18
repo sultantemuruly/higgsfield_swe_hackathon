eb9009d2c427027f4fd09b515f42b06f48ee4f45
sultantemuruly <sultantemuruly@gmail.com>
2025-10-18 22:01:18 +0500

    feat: add text_to_video, add input images for text_to_image

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from routers.text_to_image import router as text_to_image_router
+from routers.text_to_video import router as text_to_video_router
 
 app = FastAPI()
 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+
 app.include_router(
-    text_to_image_router, prefix="/text-to-image", tags=["text-to-image"]
+    text_to_image_router,
+    prefix="/text-to-image",
+    tags=["text-to-image"],
+)
+
+app.include_router(
+    text_to_video_router,
+    prefix="/text-to-video",
+    tags=["text-to-video"],
 )
